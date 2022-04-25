cb384d63b34870bb69c2fc852fa8e9579c1a8dc8
KDeeM <kadeemalexander254@gmail.com>
2022-04-25 15:19:57 +0300

    Made the code more function based and created a loader

--- app.js
+++ app.js
@@ -1,35 +1,85 @@
-// Create a new pixi application
-let app = new PIXI.Application({
-  width: 640,
-  height: 360,
-});
-
-// Add the generated view to the dom
-let pixiView = document.querySelector( "#pixiContainer" );
-pixiView.appendChild( app.view );
-
-// Load a sprite to display in the view
-let spr_spaceShip = PIXI.Sprite.from( "spaceShip_original.png" );
-
-// add the sprite to the application stage
-// the stage is a container serving as the root of the scene graph
-// all children of the stage are rendered every frame
-app.stage.addChild( spr_spaceShip );
+const sample = {};
+const SPRITES = {};
+// Add a variable to count the elapsed time
+let elapsedTime = 0.0;
 
 // Now an animation loop can be added to move our sprite, this loop is essential
 // for animation of the objects in the view
 
-// Add a variable to count the elapsed time
-let elapsedTime = 0.0;
+function init() {
+  createPixiApplication();
+  addRocket();
+  loadAssets().then(
+    (val) => {
+      console.log( PIXI.Loader.shared.resources );
+      // let ball = new PIXI.Sprite.from(  PIXI.Loader.shared.resources.image1.texture.baseTexture );
+      let ball = new PIXI.Sprite(  PIXI.Loader.shared.resources.image1.texture );
+      let width = PIXI.Loader.shared.resources.image1.texture.baseTexture.width;
+      let height = PIXI.Loader.shared.resources.image1.texture.baseTexture.height;
 
-// Tell the applications ticker to run a new callback every frame
-// we pass it delta, the amount of time that has passed between frames
-app.ticker.add(
-  ( delta )=>{
+      ball.x = width;
+      ball.y = height;
+      ball.pivot.x = width / 2;
+      ball.pivot.y = height / 2;
+      ball.angle = 90;
+      console.log(  PIXI.Loader.shared.resources.image1.texture.baseTexture.width );
+      sample.app.stage.addChild( ball );
+    }
+  )
+
+  // Tell the applications ticker to run a new callback every frame
+  // we pass it delta, the amount of time that has passed between frames
+  sample.app.ticker.add((delta) => {
     // increment the elapsed time by delta
     elapsedTime += delta;
 
     // update the sprite position
-    spr_spaceShip.x = 100 + Math.cos( elapsedTime / 50.00 ) * 100;
-  }
-)
+    SPRITES.spaceShip.x = 100 + Math.cos(elapsedTime / 50.0) * 100;
+  });
+  return;
+}
+
+function createPixiApplication() {
+  // create a pixi application
+  sample.app = new PIXI.Application({
+    width: 640,
+    height: 360,
+  });
+  // Add the generated view to the dom
+  sample.parent = document.querySelector("#pixiContainer");
+  sample.parent.appendChild(sample.app.view);
+  return;
+}
+
+function loadAssets() {
+  return new Promise(function( res, rej ){
+
+    let loader = PIXI.Loader.shared;
+    loader.add("image1", "image01.png");
+    // .add( "image2", "image02.png" )
+    // .add( "image3", "image03.png" );
+    loader.load();
+
+    console.log( "loading" );
+
+    loader.onError.add(
+      () => {
+        return rej("failed");
+      }
+    )
+
+    loader.onLoad.add(
+      () => {
+        return res("loaded");
+      }
+    )
+  })
+}
+
+function addRocket() {
+  SPRITES.spaceShip = PIXI.Sprite.from("spaceShip_original.png");
+  sample.app.stage.addChild( SPRITES.spaceShip );
+  return;
+}
+
+window.addEventListener("load", init);
